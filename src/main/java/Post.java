20d14965ed5a49995bdebcc3e15192c5e2871250
Nekrasov Dmitriy <dimannekr@mail.ru>
2022-10-31 23:31:35 +0700

    Homework API

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String patronymic;
+    public boolean subscription;
+    public String passport;
+    public String phone;
+    public String surname;
+    public FormDate birthday;
+}
